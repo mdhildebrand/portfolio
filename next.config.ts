c8e34c0e78a20fe0061d6bbb7943501f58fb7728
Matt Hildebrand <hildebrand.matt@gmail.com>
2026-04-02 13:17:48 -0700

    updating webpack build

--- next.config.ts
+++ next.config.ts
@@ -11,10 +11,21 @@ const nextConfig: NextConfig = {
     },
   },
   webpack(config) {
+    // Find and fully remove any existing SVG rules
+    config.module.rules = config.module.rules.filter((rule: any) => {
+      if (rule.test?.toString().includes('svg')) return false;
+      if (rule.oneOf) {
+        rule.oneOf = rule.oneOf.filter((r: any) => !r.test?.toString().includes('svg'));
+      }
+      return true;
+    });
+
     config.module.rules.push({
       test: /\.svg$/,
+      issuer: /\.[jt]sx?$/,
       use: ['@svgr/webpack'],
     });
+
     return config;
   },
 };
