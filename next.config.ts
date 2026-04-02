9bbedf2a1d63cb640d3cb3e86db7bf2a8cd69e56
Matt Hildebrand <hildebrand.matt@gmail.com>
2026-04-02 13:56:19 -0700

    switching production build to turbopack

--- next.config.ts
+++ next.config.ts
@@ -2,33 +2,21 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  images: {
-    dangerouslyAllowSVG: true,
-    disableStaticImages: true,
-  },
   turbopack: {
     rules: {
       '*.svg': {
-        loaders: ['@svgr/webpack'],
+        loaders: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              typescript: true,
+              dimensions: false,
+            },
+          },
+        ],
         as: '*.js',
       },
     },
-  },
-  webpack(config, { isServer }) {
-    console.log('webpack config running, isServer:', isServer);
-    console.log('SVG rules:', JSON.stringify(
-      config.module.rules.filter((rule: any) => 
-        rule.test?.toString().includes('svg')
-      ), null, 2
-    ));
-    
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    });
-
-    return config;
   },
 };
 
